823ec8a6be733beeb87d587858764ce152e40c51
Wisnu Prayogo Kusumo <71598712+WisnuPrayogo096@users.noreply.github.com>
2023-12-21 14:33:36 +0700

    Upload files

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from google.api_core import retry
+from dotenv import load_dotenv
+# Importing palm, retry functions, and other dependencies
+import google.generativeai as palm
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Configure API key
+api_key = os.getenv("GOOGLE_API_KEY")
+palm.configure(api_key=api_key)
+
+# Function to retry chat with exponential backoff
+@retry.Retry()
+def retry_chat(**kwargs):
+    return palm.chat(**kwargs)
+
+# Function to retry reply with exponential backoff
+@retry.Retry()
+def retry_reply(arg):
+    return palm.reply(arg)
+
+# Streamlit app
+def main():
+    st.title("Abstract Summarizer with PaLM 2")
+
+    # Get available models
+    models = [m.name for m in palm.list_models() if 'generateMessage' in m.supported_generation_methods]
+
+    # Model selection dropdown
+    selected_model = st.selectbox("Select a model:", models)
+    
+    # Default question text
+    default_question = "hello, I need a short summary and Indonesian translation of one paragraph. Can you provide a professional Indonesian translation and a short summary? :"
+
+    # Input for user question
+    question = st.text_area("Ask a question:", value=default_question, height=300)
+
+    if st.button("Submit"):
+        if question:
+            # Retry chat interaction
+            response = retry_chat(
+                model=selected_model,
+                context="You are a professional Indonesian translator and good at summarizing a paragraph.",
+                messages=question,
+            )
+
+            # Display response
+            st.text("Response:")
+            st.write(response.last)
+        
+if __name__ == "__main__":
+    main()
